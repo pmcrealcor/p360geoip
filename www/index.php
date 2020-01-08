d2239f36a7953c5ff0f04010c969d65bad6df9ef
Paulo Almeida <palmeida@growin.pt>
2020-01-08 14:42:58 +0000

    main application file

--- www/index.php
+++ www/index.php
@@ -12,14 +12,12 @@ include_once("../Model.php");
 include_once("../Router.php");
 include_once("../Response.php");
 
-
-/*
 $response = (new Router())->execute();
 $response->handover();
-*/
 
-
+/*
 include_once("../Models/Builder.php");
 $builder = new Builder();
-//$builder->createTable();
+$builder->createTable();
 $builder->importData();
+*/
